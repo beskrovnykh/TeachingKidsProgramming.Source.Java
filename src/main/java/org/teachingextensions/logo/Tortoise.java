768082a7956c89bf60eeb2e523dad1bba0ed2e68
Andrey Beskrovnykh <abeskrov@cisco.com>
2017-10-05 00:57:10 +0700

    Change eclipse page encoding and make translation for Tortoise API

--- src/main/java/org/teachingextensions/logo/Tortoise.java
+++ src/main/java/org/teachingextensions/logo/Tortoise.java
@@ -12,14 +12,14 @@ import org.teachingextensions.logo.utils.PizzaUtils.Topping;
 import org.teachingextensions.virtualproctor.VirtualProctor;
 
 /**
- * <img src="https://lh5.googleusercontent.com/-B3Q59gpYW8o/T4tA2k_TYUI/AAAAAAAAAjo/WiqdoXjbkb0/s65/Tortoise.png" style="text-align: left" alt="A tortoise drawing a line" >
- * The Tortoise allows you to draw lines and shapes by moving it around on the window
+ * <img src="https://lh5.googleusercontent.com/-B3Q59gpYW8o/T4tA2k_TYUI/AAAAAAAAAjo/WiqdoXjbkb0/s65/Tortoise.png" style="text-align: left" alt="Черепашка рисующая линию" >
+ * Черепашка (The Tortoise) умеет рисовать всевозможные линии и предметы передвигаясь по экрану
  */
 public class Tortoise
 {
   private Topping topping;
   /**
-   * Displays the Tortoise
+   * Показывает черепашку
    * <div><b>Example:</b> {@code  Tortoise.show()} </div>
    */
   public static void show()
@@ -27,18 +27,18 @@ public class Tortoise
     turtle().show();
   }
   /**
-   * Gets the name for the tortoise 
-   * <div><b>Example:</b> {@code String name =  Tortoise.getName("Anita Borg");}</div>
+   * Возвращает имя черепашки
+   * <div><b>Example:</b> {@code String name =  Tortoise.getName("Маруська");}</div>
    *
-   * @return the name for the Tortoise 
+   * @return имя черепашки
    */
   public static String getName()
   {
     return turtle().getName();
   }
   /**
-   * Sets the name for the tortoise 
-   * <div><b>Example:</b> {@code  Tortoise.setName("Ada Lovelace")}</div>
+   * Устанавливает имя для черепашки
+   * <div><b>Example:</b> {@code  Tortoise.setName("Васька")}</div>
    *
    * @param name
    *          The name for the Tortoise
@@ -48,97 +48,97 @@ public class Tortoise
     turtle().setName(name);
   }
   /**
-   * Gets the speed that the tortoise moves
+   * Возвращает скорость с которой будет передвигаться черепашка
    * <div><b>Example:</b> {@code int speed =  Tortoise.getSpeed(8);}</div>
    *
-   * @return the speed the Tortoise is currently set to
+   * @return скорость с которой передвигается черепашка
    */
   public static int getSpeed()
   {
     return turtle().getSpeed();
   }
   /**
-   * Sets the speed the tortoise moves
+   * Устанавливает скорость с которой будет передвигаться черепашка
    * <div><b>Example:</b> {@code  Tortoise.setSpeed(8)}</div>
    *
    * @param speed
-   *          The speed from 1 (slowest) to 10 (fastest)
+   *          Скорость от 1 (самая медленная) до 10 (самая большая)
    */
   public static void setSpeed(int speed)
   {
     turtle().setSpeed(speed);
   }
   /**
-   * Moves the Tortoise Forward the number of pixels specified. <br>
+   * Перемещает черепашку на заданное число пикселей. <br>
    * <div><b>Example:</b> {@code Tortoise.move(72)}</div>
    *
    * @param lengthInPixels
-   *          The number of pixels to move. Negative numbers will move the
-   *          Tortoise backwards.
+   *          Число пикселей на которое нужно переместиться.
+   *          Отрицательное число будет перемещать черепашку в обратном направлении.
    */
   public static void move(Number lengthInPixels)
   {
     turtle().move(lengthInPixels);
   }
   /**
-   * Turns the Tortoise to the right (clockwise) the degrees specified. <br>
+   * Поворачивает черепашку на указанное число пикселей направо (по часовой стрелке)<br>
    * <div><b>Example:</b> {@code Tortoise.turn(90)}</div>
    *
-   * @param degreesToTheRight
-   *          The degrees to turn. Negative numbers will move the Tortoise to
-   *          the left (counter-clockwise)
+   * @param degreesToTheRight по часовой стрелке
+   *          Число градусов на которое нужно повернуться. Отрицательное число будет
+   *          поворачивать черепашку налево (против часовой стрелки)
    */
   public static void turn(Number degreesToTheRight)
   {
     turtle().turn(degreesToTheRight.doubleValue());
   }
   /**
-   * Gives you access to the window the
-   * Tortoise is moving on so you can do things like change it's color. <br>
+   * Дает возможность управлять холстом, по которому перемещается черепашка.
+   * Используя холст, вы сможете изменить цвет фона. </br>
    * <b>Example:</b> {@code  TurtlePanel panel = Tortoise.getBackgroundWindow()}
-   * @return the (program) window that the Tortoise is moving on
+   * @return окно программы, по которому перемещается черепашка.
    */
   public static TurtleWindow getBackgroundWindow()
   {
     return turtle().getBackgroundWindow();
   }
   /**
-   * Gets the current width of the pen drawn by the Tortoise. <br>
+   * Возвращает ширину линии пера, которым рисует черепашка <br>
    * <div><b>Example:</b> {@code  width = Tortoise.getPenWidth()}</div>
    *
-   * @return the width of the pen stroke
+   * @return ширина линии пера
    */
   public static int getPenWidth()
   {
     return turtle().getPenWidth();
   }
   /**
-   * Sets the width of the pen drawn by the Tortoise. <br>
+   * Устанавливает ширину линии пера, которым рисует черепашка <br>
    * <div><b>Example:</b> {@code  Tortoise.setPenWidth(2)}</div>
    *
    * @param width
-   *          the width of the pen stroke
+   *          ширина линии
    */
   public static void setPenWidth(Number width)
   {
     turtle().setPenWidth(width.intValue());
   }
   /**
-   * Gets the current color of the pen drawn by the Tortoise. <br>
+   * Возвращает цвет пера, которым рисует черепашка <br>
    * <div><b>Example:</b> {@code  pen = Tortoise.getPenColor()}</div>
    *
-   * @return the color of the pen stroke
+   * @return цвет линии
    */
   public static Color getPenColor()
   {
     return turtle().getPenColor();
   }
   /**
-   * Sets the color of the line drawn by the Tortoise. <br>
+   * Задает цвет пера, которым рисует черепашка<br>
    * <div><b>Example:</b> {@code  Tortoise.setPenColor(PenColors.Reds.Red)}</div>
    *
    * @param color
-   *          the color of the line drawn 
+   *          цвет нарисованной линии
    * @see PenColors
    */
   public static void setPenColor(Color color)
@@ -146,80 +146,83 @@ public class Tortoise
     turtle().setPenColor(color);
   }
   /**
-   * Gets the current position of the Tortoise on the y axis. <br>
-   * (0,0) is the top left of the screen <br>
+   * Возвращает текущее положении черепашки по оси ординат Y.<br>
+   * (0,0) - координаты верхнего левого угла экрана<br>
    * <div><b>Example:</b> {@code  y = Tortoise.getY()}</div>
    *
-   * @return y the position in pixels of the Tortoise on the Y axis
+   * @return значение координаты Y черепашки в пикселях
    */
   public static int getY()
   {
     return turtle().getY();
   }
   /**
-   * Sets the position of the Tortoise on the y axis. <br>
-   * (0,0) is the top left of the screen <br>
+   * Устанавливает текущее положении черепашки по оси ординат Y.<br>
+   * (0,0) - координаты верхнего левого угла экрана<br>
    * <div><b>Example:</b> {@code  Tortoise.setY(30);}</div>
    *
    * @param y
-   *          the position in pixels of the Tortoise on the Y axis
+   *           значение координаты Y черепашки в пикселях
    */
   public static void setY(int y)
   {
     turtle().setY(y);
   }
   /**
-   * Gets the current position of the Tortoise on the x axis. <br>
-   * (0,0) is the top left of the screen <br>
+   * Возвращает текущее положении черепашки по оси абсцисс X.<br>
+   * (0,0) - координаты верхнего левого угла экрана<br>
    * <div><b>Example:</b> {@code  x = Tortoise.getX()}</div>
    *
-   * @return x the position in pixels of the Tortoise on the X axis
+   * @return значение координаты X черепашки в пикселях
    */
   public static int getX()
   {
     return turtle().getX();
   }
   /**
-   * Sets the position of the Tortoise on the x axis. <br>
-   * (0,0) is the top left of the screen <br>
+   * Устанавливает текущее положении черепашки по оси абсцисс X.<br>
+   * (0,0) - координаты верхнего левого угла экрана<br>
    * <div><b>Example:</b> {@code  Tortoise.setX(30);}</div>
    *
    * @param x
-   *          the position in pixels of the Tortoise on the X axis
+   *          значение координаты X черепашки в пикселях
    */
   public static void setX(int x)
   {
     turtle().setX(x);
   }
   /**
-   * Gets the current heading of the Tortoise. <br>
-   * 0 degrees is due north. <br>
-   * 90 degrees is due east. <br>
+   * Возвращает угол (направление) черепашки.<br>
+   * 0 градусов - север. <br>
+   * 90 градусов - восток. <br>
    * <div><b>Example:</b> {@code  angle = Tortoise.getAngleInDegrees()}</div>
    *
-   * @return the angle in degrees of the Tortoise
+   * @return направление черепашки (угол в градусах)
    */
   public static double getAngle()
   {
     return turtle().getAngleInDegrees();
   }
   /**
-   * Sets the angle the Tortoise is facing. <br>
-   * 0 is straight up (like 'North') <br>
+   * Устанавливает угол (направление) черепашки.<br>
+   * 0 градусов - север. <br>
+   * 90 градусов - восток. <br>
    * <div><b>Example:</b> {@code  Tortoise.setAngle(42);}</div>
    *
-   * @param angle
-   *          the angle in degrees
+   * @param
+   *          angle
+   *              направление черепашки (угол в градусах)
    */
   public static void setAngle(int angle)
   {
     turtle().setAngleInDegrees(angle);
   }
   /**
-   * Changes the type of animal you are using. <br>
+   * Изменяет тип зверька, которым вы управляете.<br>
    * <div><b>Example:</b> {@code  Tortoise.setAnimal(Animals.Spider);}</div>
    *
-   * @param animal the animal you want to use
+   * @param animal
+   *               зверек которым вы хотите управлять
    * @see Animals
    */
   public static void setAnimal(Animals animal)
@@ -227,7 +230,7 @@ public class Tortoise
     turtle().setAnimal(animal);
   }
   /**
-   * Blows up your turtle! <br>
+   * Делает БУМ! <br>
    * <div><b>Example:</b> {@code  Tortoise.explode(Animals.ExplodedTurtle);}</div>
    *
    * @see Animals
@@ -237,7 +240,7 @@ public class Tortoise
     turtle().setAnimal(Animals.ExplodedTurtle);
   }
   /**
-   * Makes it so the tortoise will not draw a line of color out of its butt. <br>
+   * Делает так чтобы черепашка перестала рисовать цветом.<br>
    * <b>Example:</b> {@code  Tortoise.penUp()}
    */
   public static void setPenUp()
@@ -245,7 +248,7 @@ public class Tortoise
     turtle().penUp();
   }
   /**
-   * Makes it so a line of color out of will trail from the Tortoise. <br>
+   * Делает так чтобы черепашка могла рисовать цветом.<br>
    * <b>Example:</b> {@code  Tortoise.penDown()}
    */
   public static void setPenDown()
@@ -253,7 +256,7 @@ public class Tortoise
     turtle().penDown();
   }
   /**
-   * Removes everything from the window. <br>
+   * Удаляет все с экрана.<br>
    * <b>Example:</b> {@code  Tortoise.clearWindow()}
    */
   public static void clear()
@@ -262,7 +265,7 @@ public class Tortoise
     turtle().getBackgroundWindow().setTurtle(turtle());
   }
   /**
-   * Hides the tortoise, you will still see the pen markings it made before and after it's hidden. <br>
+   * Прячет черепашку. Вы сможете увидеть то, что нарисовали до этого. <br>
    * <b>Example:</b> {@code  Tortoise.hide()}
    */
   public static void hide()
@@ -274,13 +277,13 @@ public class Tortoise
     return TortoiseUtils.getTurtle();
   }
   /**
-   * Moves the Tortoise to a particular spot on the canvas. <br>
+   * Переместить черепашку в определенную точку экрана. <br>
    * <div><b>Example:</b> {@code  Tortoise.moveTo(100,200);}</div>
    *
    * @param x
-   *          the x position
+   *          координата по оси абсцисс X
    * @param y
-   *          the y position
+   *          координата по оси ординат Y
    */
   public static void moveTo(int x, int y)
   {
@@ -291,23 +294,23 @@ public class Tortoise
     return new TurtleWindow();
   }
   /**
-   * Makes a cool shape fast
-   * <div><b>Example: </b> {@code tortoise.drawShape(6,PenColors.Reds.Red, 50, 20)}</div>
+   * Быстро рисует крутую фигурку
+   * <div><b>Example:</b> {@code tortoise.drawShape(6,PenColors.Reds.Red, 50, 20)}</div>
    * @param sides
-   *            the number of sides
+   *            число сторон
    * @param color
-   *            a snazzy line color
+   *           шикарный цвет
    * @param length
-   *            the bigger the better
+   *           чем больше тем лучше
    * @param width
-   *            make a thick line - it's cool
+   *           сделай линию потолще - будет круто!
    */
   public static void drawShape(int sides, Color color, int length, int width)
   {
     Tortoise.show();
     Tortoise.setSpeed(7);
     Tortoise.getBackgroundWindow().getCanvas().setBackground(PenColors.Yellows.Goldenrod);
-    new Text("TKP Java - Make Some Shapes!").setTopLeft(225, 50).addTo(Tortoise.getBackgroundWindow());
+    new Text("TKP Java - Нарисуй мне что-нибудь!").setTopLeft(225, 50).addTo(Tortoise.getBackgroundWindow());
     for (int i = 0; i < sides; i++)
     {
       Tortoise.setPenColor(color);
@@ -315,19 +318,19 @@ public class Tortoise
       Tortoise.move(length);
       Tortoise.turn(360 / sides);
     }
-    VirtualProctor.setClassName("Grace Hopper's Class");
-    VirtualProctor.setName("Jean Bartik");
+    VirtualProctor.setClassName("Выдающиеся дети");
+    VirtualProctor.setName("Жан Бартик");
   }
   /**
-   * Draws an entire Tortoise --  fast!
-   * <div><b>Example: </b> {@code tortoise.drawTortoise()}</div>
+   * Рисует настоящую черепашку - быстро!
+   * <div><b>Example:</b> {@code tortoise.drawTortoise()}</div>
    */
   public static void drawTortoise()
   {
     Tortoise.show();
     Tortoise.setSpeed(9);
     Tortoise.getBackgroundWindow().setBackground(PenColors.Greens.DarkSeaGreen);
-    new Text("TKP Java - It's the Tortoise!").setTopLeft(200, 75).addTo(Tortoise.getBackgroundWindow());
+    new Text("TKP Java - это черепашка!").setTopLeft(200, 75).addTo(Tortoise.getBackgroundWindow());
     Tortoise.setPenColor(PenColors.Greens.Green);
     Tortoise.setPenWidth(3);
     makeTortoiseBody();
@@ -374,12 +377,11 @@ public class Tortoise
     turtle().setPanelVisible(b);
   }
   /**
-   * Checks if a tortoise can eat a slice of a pizza
+   * Может ли ваша черепашка съесть этот кусочек пиццы
    * <div><b>Example:</b> {@code  tortoise.eatPizza(pizza)}</div>
-   *
    * @param pizza
-   *            the pizza
-   * @return whether or not there is pizza left to eat that a tortoise likes
+   *            кусочек пиццы
+   * @return может ваша черепашка съесть этот кусочек пиццы или нет
    */
   public boolean eatPizza(Pizza pizza)
   {
@@ -389,11 +391,11 @@ public class Tortoise
     return pizza.wasCooked() && pizza.hasTopping(topping);
   }
   /**
-   * Checks to see if a tortoise likes a particular kind of pizza topping
+   * Считает ли ваша черепашка данное блюдо восхитительным
    * <div><b>Example:</b> {@code  tortoise.likesTopping(topping)}</div>
    *
    * @param topping
-   *            the topping
+   *            восхитительное блюдо
    */
   public void likesTopping(Topping topping)
   {
